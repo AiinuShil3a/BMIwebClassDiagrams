ef7c1a117ab8eac98e152d779df10db32c2d36e1
AiinuShil3a <Shil3aiinu@outllok.com>
2023-07-10 22:45:45 +0700

    V.2.1

--- controller/bmiCalculator.php
+++ controller/bmiCalculator.php
@@ -13,22 +13,26 @@ if(isset($_POST['Submit'])){
 }
 
 if(isset($BMI)){
-        if($BMI <= 18.50){
+        if($BMI <= 18.50){ ?>
+                <br><br><img src="asset/img/underweight.jpg" style="width: 250px;" alt="underweight"><br><br> <?php
                 echo "<p>ส่วนสูง: {$Height} CM</p>";
                 echo "<p>น้ำหนัก: {$Weight} KG</p>";
                 echo "<p>BMI: {$BMI}</p>";
                 echo "<p>รูปร่าง: คุณผอมเกินไป</p>";
-        }else if($BMI >= 18.6 && $BMI <= 24.0){
+        }else if($BMI >= 18.6 && $BMI <= 24.0){ ?>
+                 <br><br><img src="asset/img/normalweight.jpg" style="width: 250px;" alt="normalweight"><br><br> <?php
                 echo "<p>ส่วนสูง: {$Height} CM</p>";
                 echo "<p>น้ำหนัก: {$Weight} KG</p>";
                 echo "<p>BMI: {$BMI}</p>";
                 echo "<p>รูปร่าง: คุณน้ำหนักตามมาตรฐาน</p>";
-        }else if($BMI >= 25.0 && $BMI <= 29.9){
+        }else if($BMI >= 25.0 && $BMI <= 29.9){ ?>
+                 <br><br><img src="asset/img/fat.jpg" style="width: 250px;" alt="fat"><br><br> <?php
                 echo "<p>ส่วนสูง: {$Height} CM</p>";
                 echo "<p>น้ำหนัก: {$Weight} KG</p>";
                 echo "<p>BMI: {$BMI}</p>";
                 echo "<p>รูปร่าง: คุณอ้วน</p>";
-        }else{
+        }else{  ?>
+                 <br><br><img src="asset/img/veryfat.png" style="width: 250px;" alt="veryfat"><br><br> <?php
                 echo "<p>ส่วนสูง: {$Height} CM</p>";
                 echo "<p>น้ำหนัก: {$Weight} KG</p>";
                 echo "<p>BMI: {$BMI}</p>";
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <style>
         body {
-        background-image: url('https://wallpaperaccess.com/full/5927911.gif');
+        background-image: url('asset/img/BGGIF.gif');
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: 100% 100%;
@@ -18,43 +18,47 @@
         h1 {
             color: #FFFFFF;
         }
+        .blur  {
+            backdrop-filter: blur(10px);
+        }
+        .centered {  
+            position: fixed;  
+            top: 50%;  
+            left: 50%;  
+            transform: translate(-50%, -50%);  
+        }
+        .textside {
+            font-size: 20px;
+        }
     </style>
 </head>
 <body>
-    <br>
-    <br>
-    <br>
-    <center>
-        <h1>BMI Callulator</h1>
-    </center>
-    <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-xl-10">
-                        <div class="card rounded-3 text-black">
-                            <div class="row g-0">
-                                <div class="col-lg-6">
-                                    <div class="card-body p-md-5 mx-md-4">
-                                        <div class="text-center">
-                                            <img src="https://dynamic.brandcrowd.com/asset/logo/d5ff2558-a87f-4c51-8908-6a415e3a0f83/logo-search-grid-1x?logoTemplateVersion=4&v=638228442355030000&text=BMI&colorpalette=purple" style="width: 200px;" alt="logo">
-                                        </div>
-                                        <form method="POST" action="<?=$_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
-                                            <div class="form-outline mb-4">
-                                                <input type="number" id="Height" name="Height" class="form-control" placeholder="ส่วนสูง" required />
-                                            </div>
-                                            <div class="form-outline mb-4">
-                                                <input type="number" id="Weight" name="Weight" class="form-control" placeholder="น้ำหนัก" required />
-                                            </div>
-                                                <br>
-                                                <center>
-                                                    <button type="Submit" class="btn btn-outline-primary" name="Submit">คำนวณค่า BMI</button>
-                                                </center>
-                                    </div>
-                                </div>
+    <!-- CLASS CARD -->
+    <div class="container py-5 h-100 centered">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-xl-10 blur">
+                <div class="row g-0">
+                    <div class="col-lg-6">
+                        <div class="card-body p-md-5 mx-md-4">
+                            <form method="POST" action="<?=$_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
+                                <center>
+                                    <img src="asset/img/RobotBMI.png" style="width: 250px;" alt="logo">
+                                </center>
+                            <div class="form-outline mb-4">
+                                <input type="number" id="Height" name="Height" class="form-control" placeholder="ส่วนสูง" required />
+                            </div>
+                            <div class="form-outline mb-4">
+                                <input type="number" id="Weight" name="Weight" class="form-control" placeholder="น้ำหนัก" required />
+                            </div>
+                            <br>
+                                <center>
+                                    <button type="Submit" class="btn btn-danger" name="Submit">คำนวณค่า BMI</button>
+                                </center>
+                            </div>
+                            </div>
                                 <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                    <div class="text-dark px-3 py-4 p-md-5 mx-md-4">
-                                        <br>
-                                        <br>
-                                        <br>
+                                &emsp;
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
                                         <?php include 'controller/bmiCalculator.php';?>
                                     </div>
                                 </div>
@@ -63,5 +67,8 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    <!-- ENDCLASS -->
 </body>
 </html>
